3cd159814ea42a81698cfcde52c27c6c0e7472e6
JO$E <102940954+josedani11115@users.noreply.github.com>
2023-08-22 21:20:52 -0500

    changes

--- U.S. Unemployment Rates.py
+++ U.S. Unemployment Rates.py
@@ -7,17 +7,14 @@ import seaborn as sns
 import warnings
 from typing import Union, List, Any, Dict
 import os
+from matplotlib import cm
+from matplotlib import colors
 
 
 # Define file paths
 df_path = r"D:\USER\Documents\U.S.-Unemployment-Rates\df_sex_unemployment_rates.csv"
 dr_path = r"D:\USER\Documents\U.S.-Unemployment-Rates\df_unemployment_rates.csv"
 
-# Read the CSV files into DataFrames
-df = pd.read_csv(df_path)
-dr = pd.read_csv(dr_path)
-df_info = df.info()
-dr_info = dr.info()
 
 """
 # EDA for both DataFrames
@@ -31,5 +28,69 @@ dr_render.to_file("dr_render.html")
 """
 
 
+# Read the CSV files into DataFrames
+df = pd.read_csv(df_path) #<------ By sexs 
+
+dr = pd.read_csv(dr_path)#<-------- General/age
 
 
+#Rows
+dr_head = dr.head()
+df_head = df.head()
+
+print(df.info(), dr.info())
+
+
+
+
+# Transforming date for get asimilite as a datetime
+df['date'] = pd.to_datetime(df['date'])
+dr['date'] = pd.to_datetime(dr['date'])
+
+
+# Calculate the mean overall unemployment rate for men and women
+men_mean_rate = df['men_rate'].mean()
+women_mean_rate = df['women_rate'].mean()
+
+# Create a figure and axis
+plt.figure(figsize=(8, 8))
+
+# Create a pie chart
+plt.pie([men_mean_rate, women_mean_rate], labels=['Men', 'Women'], autopct='%1.1f%%', startangle=140)
+
+# Add a title
+plt.title('Overall Unemployment Rate Comparison (Men vs. Women)')
+
+# Display the pie chart
+plt.show()
+
+
+
+
+
+#Create a figure and axis
+plt.figure(figsize=(15,8))
+#Create a plot over time
+plt.plot(df['date'], df['men_rate'], label = 'men unemployment rate' , color = 'red' , linewidth=2)
+plt.plot(df['date'], df['women_rate'], label = 'women unemployment rate' , color = 'black' , linewidth=2)
+plt.xlabel('Date')
+plt.ylabel('Unemployment Rate (%)')
+plt.title('Unemployment Rates for Men vs. Women Over Time')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+
+#Create a average unemployment rate 
+
+plt.figure(figsize=(15,8))
+plt.plot(dr['date'], dr['overall_rate'], label ='General Unemployment rate' , color = 'black' , linewidth = 2)
+plt.xlabel('date') 
+plt.ylabel('Unemployment Rate (%)')
+plt.title('Unemployment Rates')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.legend()
+plt.tight_layout()
+plt.show()
